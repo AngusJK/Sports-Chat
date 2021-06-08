99f9c276f535b8d1516798a9fc0f1d66080f031a
Angus Kellett <anguskellett@hotmail.com>
2021-06-08 22:09:09 +0000

    requires express and creates app variable

--- index.js
+++ index.js
@@ -1,11 +1,15 @@
-const app = require('express')();
+const express = require('express');
+const app = express();
 const http = require('http').Server(app);
 const io = require('socket.io')(http);
 const PORT = process.env.PORT || 3000;
+const path = require('path');
 
-app.get('/', (req, res) => {
-  res.sendFile(__dirname + '/public/index.html');
-});
+app.use(express.static(path.join(__dirname, 'public')));
+
+// app.get('/', (req, res) => {
+//   res.sendFile(__dirname + '/public/index.html');
+// });
 
 io.on('connection', (socket) => {
   socket.on('chat message', (msg) => {
