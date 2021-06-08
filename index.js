1201bf71a5d5f5e6c23a97d10c65815aee2b70ca
Angus Kellett <anguskellett@hotmail.com>
2021-06-08 19:34:17 +0000

    refactors requires for fewer lines of code

--- index.js
+++ index.js
@@ -1,24 +1,19 @@
-const express = require('express');
-const app = express();
-const http = require('http');
-const server = http.createServer(app);
-const { Server } = require('socket.io');
-const io = new Server(server);
+const app = require('express')();
+const http = require('http').Server(app);
+const io = require('socket.io')(http);
+const PORT = process.env.PORT || 3000;
 
 app.get('/', (req, res) => {
   res.sendFile(__dirname + '/public/index.html');
 });
 
 io.on('connection', (socket) => {
-  console.log('A user connected...');
-  socket.on('disconnect', () => {
-    console.log('A user has disconnected...');
+  socket.on('chat message', (msg) => {
+    console.log('message: ' + msg);
   });
 });
 
-const PORT = 3000;
-
-server.listen(PORT, () => {
+http.listen(PORT, () => {
   console.log(`Chat app listening on port ${PORT}...`);
 });
 
