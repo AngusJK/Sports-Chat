ad9cbed2ccf01e4c9ecd1dcff99eae774e33a8df
Angus Kellett <anguskellett@hotmail.com>
2021-07-16 19:33:54 +0000

    creates saveMessage and getMessages functions

--- utils/messages.js
+++ utils/messages.js
@@ -1,5 +1,7 @@
 const moment = require('moment');
 
+let messages = [];
+
 function formatMessage(username, id, text) {
   return {
     username,
@@ -9,4 +11,14 @@ function formatMessage(username, id, text) {
   }
 }
 
-module.exports = formatMessage;
+function saveMessage(room, username, id, text) {
+  let msg = formatMessage(username, id, text);
+  msg.room = room;
+  messages.push(msg);
+}
+
+function getMessages(room) {
+  return messages.filter(message => message.room === room);
+}
+
+module.exports = { formatMessage, saveMessage, getMessages };
