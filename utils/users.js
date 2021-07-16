d329224fc1f8beec1aa725e37a983a31c7561c1c
Angus Kellett <anguskellett@hotmail.com>
2021-07-16 18:29:00 +0000

    modifies userLeave to properly remove current user from users array

--- utils/users.js
+++ utils/users.js
@@ -13,18 +13,18 @@ function getUser(id) {
 function getNumberOfUsers(room) {
   let currentRoomUsers = users.filter(user => user.room === room);
   return currentRoomUsers.length;
-}
+};
 
 function userLeave(id) {
   const index = users.findIndex(user => user.id === id);
   if(index != -1) {
-    return users.splice(index, 1);
+    users.splice(index, 1);
   }  
-}
+};
 
 module.exports = {
   userJoin,
   getUser,
   getNumberOfUsers,
   userLeave
-}
+};
