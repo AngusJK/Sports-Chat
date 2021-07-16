59ffee710cce050b701c7820dda370e02446a5fd
Angus Kellett <anguskellett@hotmail.com>
2021-07-16 00:56:59 +0000

    removes comments and unused code, moves functions to top

--- public/chat.js
+++ public/chat.js
@@ -3,49 +3,11 @@ const { username, room } = Qs.parse(location.search, {
   ignoreQueryPrefix: true
 });
 
-let currentUserId = '';
-
-
 var messages = document.getElementById('message-container');
 var chatform = document.getElementById('chat-form');
 var chatinput = document.getElementById('chat-input');
-//var signinform = document.getElementById('sign-in-form');
-//var signininput = document.getElementById('sign-in-input');
-//var signinbutton = document.getElementById('sign-in-submit');
 var clients = document.getElementById('clients');
 var roomName = document.getElementById('room-name');
-let currentUser = '';
-
-/*
-signinform.addEventListener('submit', (e) => {
-  e.preventDefault();
-  socket.emit('new-user', { username: signininput.value, id: socket.id });
-  signininput.value = '';
-  signininput.disabled = true;
-  signinbutton.disabled = true;
-});
-*/
-// adds event listener to form
-
-socket.emit('join-room', { username, room });
-
-
-chatform.addEventListener('submit', (e) => {
-  // prevents form from automatically submitting to a file
-  e.preventDefault();
-  // if there is data in the form, emit a chat message to the server along with clients socket id
-  if (chatinput.value) {
-    socket.emit('chat message', { text: chatinput.value, id: socket.id });
-    chatinput.value = '';
-  }
-});
-
-// creates a list element which contains the submitted message and appends it to the document
-// function addMessageToHTML(message) {
-//   const item = document.createElement('li');
-//   item.textContent = message.text;
-//   messages.append(item);
-// }
 
 function addMessageToHTML(message) {
   let selfClass = "";
@@ -58,7 +20,20 @@ function addMessageToHTML(message) {
   messages.scrollTop = messages.scrollHeight;
 };
 
-// listens for chat message to be emitted from server and calls method to handle it
+function alertUserConnected() {
+  addMessageToHTML("User connected");
+};
+
+chatform.addEventListener('submit', (e) => {
+  e.preventDefault();
+  if (chatinput.value) {
+    socket.emit('chat message', { text: chatinput.value, id: socket.id });
+    chatinput.value = '';
+  }
+});
+
+socket.emit('join-room', { username, room });
+
 socket.on('chat message', (message) => { 
   addMessageToHTML(message);
 });
@@ -70,24 +45,13 @@ socket.on('new-user', (data) => {
   messages.appendChild(div);
 });
 
-// passes user connected alert to addMessageToHTML method for rendering
-function alertUserConnected() {
-  addMessageToHTML("User connected");
-};
-
-// listens for user connected to be emitted from server, calls alert user method
 socket.on('user connected', alertUserConnected);
 
-// listens for newClientConnect from server, creates list element to render data
 socket.on('newClientConnect', (data) => {
   clients.innerHTML = data.description;
-  // const item = document.createElement('li');
-  // item.textContent = data.description;
-  // messages.append(item);
 });
 
 socket.on('connectToRoom', (data) => {
   addMessageToHTML(data);
   roomName.innerHTML = room;
 });
-// window.scrollTo(0, document.body.scrollHeight);
