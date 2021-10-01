6f9e0ddcbe0d3ebb96935ec04a22c09562252bdb
Angus Kellett <anguskellett@hotmail.com>
2021-10-01 03:50:04 +0000

    defines io by requiring socket.io, refomats function syntax, removes semicolons

--- public/chat.js
+++ public/chat.js
@@ -1,83 +1,84 @@
-const socket = io();
+const io = require('socket.io')
+const socket = io()
 const { username, room } = Qs.parse(location.search, {
   ignoreQueryPrefix: true
-});
+})
 
-var messages = document.getElementById('message-container');
-var chatform = document.getElementById('chat-form');
-var chatinput = document.getElementById('chat-input');
-var clients = document.getElementById('clients');
-var roomName = document.getElementById('room-name');
-var leave = document.getElementById('leave');
+const messages = document.getElementById('message-container')
+const chatform = document.getElementById('chat-form')
+const chatinput = document.getElementById('chat-input')
+const clients = document.getElementById('clients')
+const roomName = document.getElementById('room-name')
+const leave = document.getElementById('leave')
 
-function addMessageToHTML(message) {
-  let selfClass = "";
-  if(message.id === socket.id) {selfClass = "self"};
-  const div = document.createElement('div');
-  div.classList.add('message');
+const addMessageToHTML = function (message) {
+  let selfClass = ''
+  if (message.id === socket.id) { selfClass = 'self' }
+  const div = document.createElement('div')
+  div.classList.add('message')
   div.innerHTML = `<span class="post ${selfClass}"><p class="meta">${message.username} <span>${message.time}</span></p>
-    <p class="text">${message.text}</p></span><br>`;
-  messages.appendChild(div);
-  messages.scrollTop = messages.scrollHeight;
-};
+    <p class="text">${message.text}</p></span><br>`
+  messages.appendChild(div)
+  messages.scrollTop = messages.scrollHeight
+}
 
-function alertUserConnected() {
-  addMessageToHTML("User connected");
-};
+const alertUserConnected = function () {
+  addMessageToHTML('User connected')
+}
 
 chatform.addEventListener('submit', (e) => {
-  e.preventDefault();
+  e.preventDefault()
   if (chatinput.value) {
-    socket.emit('chat message', { text: chatinput.value, id: socket.id });
-    chatinput.value = '';
+    socket.emit('chat message', { text: chatinput.value, id: socket.id })
+    chatinput.value = ''
   }
-});
+})
 
-socket.emit('join-room', { username, room });
+socket.emit('join-room', { username, room })
 
-socket.on('chat message', (message) => { 
-  addMessageToHTML(message);
-});
+socket.on('chat message', (message) => {
+  addMessageToHTML(message)
+})
 
 socket.on('new-user', (data) => {
-  if (data.user.username != username) {
-  const div = document.createElement('div');
-  div.classList.add('admin-message');
-  div.innerHTML = `<p class="meta">${data.msg}</p>`;
-  messages.appendChild(div);
+  if (data.user.username !== username) {
+    const div = document.createElement('div')
+    div.classList.add('admin-message')
+    div.innerHTML = `<p class="meta">${data.msg}</p>`
+    messages.appendChild(div)
   }
-});
+})
 
-socket.on('user connected', alertUserConnected);
+socket.on('user connected', alertUserConnected)
 
 socket.on('newClientConnect', (data) => {
-  clients.innerHTML = data.description;
-});
+  clients.innerHTML = data.description
+})
 
 socket.on('connectToRoom', (data) => {
-  addMessageToHTML(data);
+  addMessageToHTML(data)
   let roomWithEmoji = ''
-  if (room === "Baseball") {
-    roomWithEmoji = "⚾️  BASEBALL";
-  } else if (room === "Basketball") {
-    roomWithEmoji = "🏀  BASKETBALL";
-  } else if (room === "Hockey") {
-    roomWithEmoji = "🏒  HOCKEY";
-  } else if (room === "Soccer") {
-    roomWithEmoji = "⚽️  SOCCER";
-  } else if (room === "Tennis") {
-    roomWithEmoji = "🎾  TENNIS"
+  if (room === 'Baseball') {
+    roomWithEmoji = '⚾️  BASEBALL'
+  } else if (room === 'Basketball') {
+    roomWithEmoji = '🏀  BASKETBALL'
+  } else if (room === 'Hockey') {
+    roomWithEmoji = '🏒  HOCKEY'
+  } else if (room === 'Soccer') {
+    roomWithEmoji = '⚽️  SOCCER'
+  } else if (room === 'Tennis') {
+    roomWithEmoji = '🎾  TENNIS'
   }
-  roomName.innerHTML = roomWithEmoji;
-});
+  roomName.innerHTML = roomWithEmoji
+})
 
 socket.on('past messages', (msgs) => {
-  console.log(msgs);
-  for (let msg of msgs) {
-    addMessageToHTML(msg);
+  console.log(msgs)
+  for (const msg of msgs) {
+    addMessageToHTML(msg)
   }
-});
+})
 
 leave.addEventListener('click', () => {
-  window.location = './index.html';
-});
+  window.location = './index.html'
+})
